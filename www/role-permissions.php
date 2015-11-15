7410e43d0c1a6f7a4faa666548f83fc0b896ffcf
Loustik <nicolas.lethellier@gmail.com>
2015-11-15 19:52:54 +0100

    assignation

--- www/role-permissions.php
+++ www/role-permissions.php
@@ -17,141 +17,152 @@
 <body>
 
 	<?php include 'header.php'; ?>
-	<?php $undeletableRoles=array("Admin"); ?>
+	<?php $rbac = new Rbac(); ?>
 
 	<ol class="breadcrumb">
 		<li><a href="/">Home</a></li>
 		<li><a href="#">Administration</a></li>
-		<li class="active">Modification de rôle</li>
+		<li><a href="/role-manage.php">Gestion des rôles</a></li>
+		<li class="active">Attributions de permissions</li>
 	</ol>
 
+	<script type="text/javascript">
+		function send(pID) {
+			document.getElementById('permissionID').value=pID;
+			document.forms["permrole"].submit();
+		}
+	</script>
 
 
-	<!-- Update permissions of a role : Controller -->
-	<?php
-		if (isset($_POST['roleID'])){
-			$id = str_replace("'","", $_POST['roleID']);
-			if($id == ""){
-				$genericUpdateError = "Impossible de mettre à jour un rôle inconnu";
+	<!-- Common -->
+	<?php 
+		$roleID = str_replace("'","", $_POST['roleID']);
+		if($roleID == ""){
+			$roleUpdateError = "Aucun rôle défini";
+		}
+		else {
+			$check_query = "SELECT ID FROM rbac_roles WHERE ID='$roleID'" or die("Erreur lors de la consultation" . mysqli_error($link)); 
+			$verif = mysqli_query($link, $check_query);
+			$row_verif = mysqli_fetch_assoc($verif);
+			$role = mysqli_num_rows($verif);		
+			if (!$role){
+				$roleUpdateError = "Le rôle en question n'existe pas";
 			}
-			else{
-				$check_query = "SELECT ID FROM rbac_roles WHERE ID='$id'" or die("Erreur lors de la consultation" . mysqli_error($link)); 
-				$verif = mysqli_query($link, $check_query);
-				$row_verif = mysqli_fetch_assoc($verif);
-				$role = mysqli_num_rows($verif);		
-				if (!$role){
-					$genericUpdateError = "Le rôle en question n'existe pas";
+		}
+		if(!empty($roleUpdateError)) {
+			echo "<div class='alert alert-danger'><strong>Erreur</strong> : ".$roleUpdateError."</div>";
+		}
+		else {
+			$roleTitle=$rbac->Roles->getTitle($roleID);
+		?>
+
+
+		<!-- Update a role's permissions : Controller -->
+		<?php
+			if (isset($_POST['permissionID'])){
+				$permissionID = str_replace("'","", $_POST['permissionID']);
+				if($permissionID == ""){
+					$genericUpdateError = "Impossible de mettre à jour une permission inconnue";
 				}
-				else {
-					$rbac = new PhpRbac\Rbac();
-					if (isset($_POST['inputRoleTitle'])) {
-						$title=$_POST['inputRoleTitle'];
+				else{
+					$check_query = "SELECT ID FROM rbac_permissions WHERE ID='$permissionID'" or die("Erreur lors de la consultation" . mysqli_error($link)); 
+					$verif = mysqli_query($link, $check_query);
+					$row_verif = mysqli_fetch_assoc($verif);
+					$permission = mysqli_num_rows($verif);		
+					if (!$permission){
+						$genericUpdateError = "La permission en question n'existe pas";
 					}
 					else {
-						$title=$rbac->Roles->getTitle($id);
-					}
-					if (isset($_POST['inputRoleDescription'])) {
-						$description=$_POST['inputRoleDescription'];
-					}
-					else {
-						$description=$rbac->Roles->getDescription($id);
-					}
-					if (isset($_POST['updateRole'])) {	
-						$check_query = "SELECT ID FROM rbac_roles WHERE Title='$title'" or die("Erreur lors de la consultation" . mysqli_error($link)); 
+						$permissionTitle=$rbac->Permissions->getTitle($permissionID);
+						$check_query = "SELECT ID FROM rbac_roles WHERE ID='$roleID'" or die("Erreur lors de la consultation" . mysqli_error($link)); 
 						$verif = mysqli_query($link, $check_query);
 						$row_verif = mysqli_fetch_assoc($verif);
 						$role = mysqli_num_rows($verif);		
-						if ($role){
-							$genericUpdateError = "Un rôle du même titre existe déjà (".$title.")";
-							$updateErrorTitle = "Un rôle du même titre existe déjà (".$title.")";
-						}
-						
-						else if (in_array($title, $undeletableRoles)) { 
-							$genericUpdateError = "Il est interdit de mettre à jour le rôle '".$title."'";
-							$updateErrorTitle = "Il est interdit de mettre à jour le rôle '".$title."'";
+						if (!$role){
+							$genericUpdateError = "Le rôle en question n'existe pas";
 						}
 						else {
-							$perm_id = $rbac->Roles->edit($id, $title, $description);
-							if (!$perm_id){
-								$genericUpdateError = "Echec de la mise à jour (ID=".$id.")";
+							if ($rbac->Roles->hasPermission($roleID, $permissionID)) {
+								$isDone = $rbac->Roles->unassign($roleTitle, $permissionTitle);
 							}
 							else {
-								$genericUpdateSuccess = "Rôle mise à jour (".$title.")";	
+								$isDone = $rbac->Roles->assign($roleTitle, $permissionTitle);
+							}
+							if (!$isDone){
+								$genericUpdateError = "Echec de la mise à jour ('".$permissionTitle."')";
+							}
+							else {
+								$genericUpdateSuccess = "Rôle mis à jour avec la permission '".$permissionTitle."'";	
 							}
 						}
 					}
 				}
 			}
-		}
-	?>
-
-
-
-	<!-- Update permissions of a role : display form -->
-	<div class="container">
-		<?php
-			if (!empty($genericUpdateError)){
-				echo "<div class='alert alert-danger'><strong>Erreur</strong> : ".$genericUpdateError."</div>";
-			} elseif (!empty($genericUpdateSuccess)){
-				echo "<div class='alert alert-success'><strong>Effectué</strong> : ".$genericUpdateSuccess."</div>";
-			}
 		?>
 
-		<h2>Modifier les permissions d'un rôle</h2>
-		<form class="form-horizontal" action='role-edit.php' method='post' accept-charset='utf-8'>
-			<div class="panel panel-default">
-				<input type="hidden" name="updateRole">
-				<input type="hidden" name="roleID" value="<?php echo $id;?>">
-				<div class="panel-heading">
-					<h3 class="panel-title">Rôle : <?php echo $title;?></h3>
-				</div>
-				<div class="panel-body">
-					Ce rôle a <?php 
-						$rbac = new PhpRbac\Rbac();
-						$count = count($rbac->Roles->permissions($id));
-						echo $count;
-					?> permissions.
 
-					<?php if (!empty($updateErrorTitle)){ ?>
-						<div class="form-group has-error has-feedback">
-							<label for="inputRoleTitle" class="col-sm-4 control-label">Nouveau titre</label>
-							<div class="col-sm-8">
-								<input type="text" class="form-control" id="inputRoleTitle" name="inputRoleTitle" aria-describedby="inputError2Status" placeholder="Directeur Local des Opérations" value="<?php echo $title;?>">
-							</div>
-						<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>
-							<span id="inputError2Status" class="sr-only">(error)</span>
-						</div>
-					<?php } else { ?>
+
+		<!-- Update a role's permissions : display form -->
+		<div class="container">
+			<?php
+				if (!empty($genericUpdateError)){
+					echo "<div class='alert alert-danger'><strong>Erreur</strong> : ".$genericUpdateError."</div>";
+				} elseif (!empty($genericUpdateSuccess)){
+					echo "<div class='alert alert-success'><strong>Effectué</strong> : ".$genericUpdateSuccess."</div>";
+				}
+				
+			?>
+
+			<h2>Modifier les permissions d'un rôle</h2>
+			<form id="permrole" class="form-horizontal" action='role-permissions.php' method='post' accept-charset='utf-8'>
+				<div class="panel panel-default">
+					<input type="hidden" name="roleID" value="<?php echo $roleID;?>">
+					<input type="hidden" name="permissionID" id="permissionID" value="undefined">
+					<div class="panel-heading">
+						<h3 class="panel-title"><?php echo $roleTitle;?></h3>
+					</div>
+					<div class="panel-body">
+						Ce rôle a <?php 
+							$count = count($rbac->Roles->permissions($roleID));
+							echo $count;
+						?> permissions. Les changements se font directement en cliquant sur les boutons. 
+						<br /> <br />
+
+						<?php 
+							$query = "SELECT ID, Title, Description FROM rbac_permissions ORDER by Title ASC";
+							$permissions = mysqli_query($link, $query);
+							while($permission = mysqli_fetch_array($permissions)) { 
+								$permissionID=$permission["ID"];
+								$permissionTitle=$permission["Title"];
+								$permissionDescription=$permission["Description"];
+								
+								if ($rbac->Roles->hasPermission($roleID, $permissionID)) {
+									?>
+									<button type="button" class="btn btn-default btn-xs active" title="<?php echo $permissionDescription;?>" onClick="send(<?php echo $permissionID;?>)"><?php echo $permissionTitle;?></button>
+									<?php
+								}
+								else {
+									?>
+									<button type="button" class="btn btn-default btn-xs" title="<?php echo $permissionDescription;?>" onClick="send(<?php echo $permissionID;?>)"><?php echo $permissionTitle;?></button>
+									<?php
+								}
+
+							} ?>
+							<br /> <br />
+
+
 						<div class="form-group">
-							<label for="inputRoleTitle" class="col-sm-4 control-label">Nouveau titre</label>
-							<div class="col-sm-8">
-								<input type="text" class="form-control" id="inputRoleTitle" name="inputRoleTitle" aria-describedby="inputError2Status" placeholder="Directeur Local des Opérations" value="<?php echo $title;?>">
-							</div>
+							<div class="col-sm-offset-4 col-sm-8">
+								<a class="btn btn-info" href="role-manage.php" role="button">Retour à la liste des rôles</a>
+						    </div>
 						</div>
-					<?php } ?>
-
-					<div class="form-group">
-						<label for="inputRoleDescription" class="col-sm-4 control-label">Nouvelle description</label>
-						<div class="col-sm-8">
-							<input type="text" class="form-control" id="inputRoleDescription" name="inputRoleDescription" placeholder="Décrire l'utilité du rôle" value="<?php echo $description;?>">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-sm-offset-4 col-sm-8">
-							<?php if (empty($genericUpdateSuccess)){ ?>
-								<a class="btn btn-default" href="role-manage.php" role="button">Annuler - Retour à la liste</a>
-							<?php } ?>
-							<button type="submit" class="btn btn-warning">Mettre à jour</button>
-							<?php if (isset($_POST['updateRole']) && !empty($genericUpdateSuccess)) { ?>
-								<a class="btn btn-success" href="role-manage.php" role="button">J'ai terminé ! Retour à la liste</a>
-							<?php } ?>
-					    </div>
 					</div>
 				</div>
-			</div>
-		</form>
-	</div>
-
+			</form>
+		</div>
+	<?php
+		}
+	?>
 
 <?php include 'footer.php'; ?>
 </body>
