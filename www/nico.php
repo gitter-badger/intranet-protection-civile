17b9ab3b71413ee9accf20175ff1a902564ded8d
Loustik <nicolas.lethellier@gmail.com>
2015-11-16 02:45:43 +0100

    Test

--- www/nico.php
+++ www/nico.php
@@ -12,7 +12,7 @@ require_once ('PhpRbac/src/PhpRbac/Rbac.php');
 <?php echo date("H:i:s");
 
 use PhpRbac\Rbac;
-$rbac = new PhpRbac\Rbac();
+$rbac = new Rbac();
 
 // Create a Permission
 //$perm_id = $rbac->Permissions->add('delete_posts', 'Can delete forum posts');
@@ -24,9 +24,17 @@ $rbac = new PhpRbac\Rbac();
 // Get Entity Id's
 //$perm_id = $rbac->Permissions->returnId('delete_posts');
 
-$role_id = $rbac->Roles->returnId('forum_moderator');
-$role_title = $rbac->Roles->getTitle($role_id);
-$role_desc = $rbac->Roles->getDescription($role_id);
+//$role_id = $rbac->Roles->returnId('forum_moderator');
+//$role_title = $rbac->Roles->getTitle($role_id);
+//$role_desc = $rbac->Roles->getDescription($role_id);
+
+$perm_descriptions = array(
+    'Can delete users',
+    'Can edit user profiles',
+    'Can view users'
+);
+
+$rbac->Permissions->addPath('/delete_users/edit_users/view_users', $perm_descriptions);
 
 // Assign Permission to Role
 //$rbac->assign($role_id, $perm_id);
